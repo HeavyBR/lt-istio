15d59829ee21248572769e10180c949576987a8d
Restyled.io <commits@restyled.io>
2021-05-28 17:07:02 +0000

    Restyled by shfmt

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ kind create cluster --config=./config/kind-config.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9/manifests/namespace.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9/manifests/metallb.yaml
-sed s/.x/"$(docker network inspect -f '{{.IPAM.Config}}' kind | awk '{print $2}' | grep -Eo '\.[0-9]{2}')"/g ./config/metallb-configmap.yaml | \
-    kubectl apply -f -
+sed s/.x/"$(docker network inspect -f '{{.IPAM.Config}}' kind | awk '{print $2}' | grep -Eo '\.[0-9]{2}')"/g ./config/metallb-configmap.yaml |
+  kubectl apply -f -
 
 # install istio + addons
 istioctl install --set profile=demo --skip-confirmation
